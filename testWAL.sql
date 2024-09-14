42f35b11da198b4bf29839c09b84ac7be74c8251
darkCapacitor <adityaa38@gmail.com>
2024-09-14 17:03:02 +0530

    Update testWAL.sql

--- testWAL.sql
+++ testWAL.sql
@@ -1,73 +1,81 @@
-WHILE (@BalanceAmt < 0)
-BEGIN
-    -- Calculate interest if applicable
-    IF (@InterestAppliedToCode = '1' AND @NextInterestSequence = @CurrentSequence)
-    BEGIN
-        SET @InterestAmt = @BalanceAmt * @EffIntRate * 90 / 36500
-        SET @BalanceAmt += @InterestAmt
-        SET @NextInterestSequence += 3
-        SET @RepaymentInterest += @InterestAmt
-    END
-    ELSE
-    BEGIN
-        SET @InterestAmt = 0
-    END
+-- Create a numbers table for generating sequences
+WITH NumberSequence AS (
+    SELECT TOP (10000) -- Adjust this number based on your maximum expected iterations
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS N
+    FROM master.dbo.spt_values t1
+    CROSS JOIN master.dbo.spt_values t2
+),
 
-    -- Process repayment
-    IF (@InterestAppliedToCode <> '1' OR @Sequence = @CurrentSequence)
-    BEGIN
-        -- Calculate next payment date
-        SET @NextPayment = CASE
-            WHEN @IsFirstPayment = 1 THEN @NextPaymentDate
-            ELSE DATEADD(MONTH, @RepayPeriodMths, @NextPaymentDate)
-        END
+-- Generate all potential repayment dates
+RepaymentDates AS (
+    SELECT 
+        N,
+        DATEADD(MONTH, (N-1) * @RepayPeriodMths, @InitialPaymentDate) AS PaymentDate
+    FROM NumberSequence
+    WHERE DATEADD(MONTH, (N-1) * @RepayPeriodMths, @InitialPaymentDate) <= @ExpiryDate
+),
 
-        -- Get the next working day payment date
-        SELECT TOP 1 @NextPaymentDate = CASE
-            WHEN @ExpiryDate <= AsAtDate THEN @ExpiryDate
-            ELSE AsAtDate
-        END
-        FROM [dbo].[syn_Model_tbl_Dim_Calendar]
-        WHERE RegionCode = 'UK' 
-          AND IsWorkingDay = 1 
-          AND [Month] = MONTH(@NextPayment)
-          AND [Year] = YEAR(@NextPayment)
-        ORDER BY AsAtDate DESC
+-- Calculate working day payment dates
+WorkingDayPayments AS (
+    SELECT 
+        r.N,
+        COALESCE(
+            (SELECT TOP 1 c.AsAtDate
+             FROM [dbo].[syn_Model_tbl_Dim_Calendar] c
+             WHERE c.RegionCode = 'UK' 
+               AND c.IsWorkingDay = 1 
+               AND c.[Month] = MONTH(r.PaymentDate)
+               AND c.[Year] = YEAR(r.PaymentDate)
+               AND c.AsAtDate <= @ExpiryDate
+             ORDER BY c.AsAtDate DESC),
+            @ExpiryDate
+        ) AS PaymentDate
+    FROM RepaymentDates r
+),
 
-        -- Update balance amount
-        SET @BalanceAmt += CASE
-            WHEN @ExpiryDate > @NextPaymentDate THEN @RepaymentAmt
+-- Calculate interest applications
+InterestCalculations AS (
+    SELECT 
+        N,
+        PaymentDate,
+        CASE 
+            WHEN @InterestAppliedToCode = '1' AND (N-1) % 3 = 0
+            THEN @BalanceAmt * @EffIntRate * 90 / 36500
             ELSE 0
-        END
-        
-        -- Calculate next repayment amount
-        SET @NextRepaymentAmt = CASE
-            WHEN @ExpiryDate <= @NextPaymentDate THEN ABS(@BalanceAmt) + @RepaymentInterest
-            WHEN @BalanceAmt + @RepaymentAmt < @RepaymentAmt THEN @RepaymentAmt + @RepaymentInterest
-            ELSE ABS(@BalanceAmt) + @RepaymentInterest
-        END
+        END AS InterestAmount
+    FROM WorkingDayPayments
+),
 
-        -- Insert into repayment schedule
-        INSERT INTO DW.CAU_RepaymentsSchedules_TEMP (FacilityId, RepaymentAmount, PrincipalAmount, PaymentDate)
-        VALUES (
-            @FacilityId, 
-            @NextRepaymentAmt, 
-            CASE 
-                WHEN @BalanceAmt > 0 OR @ExpiryDate <= @NextPaymentDate THEN 0
-                ELSE ABS(@BalanceAmt)
-            END,
-            @NextPaymentDate
-        )
+-- Calculate cumulative repayments and balances
+Repayments AS (
+    SELECT 
+        N,
+        PaymentDate,
+        InterestAmount,
+        @RepaymentAmt AS RepaymentAmount,
+        SUM(InterestAmount) OVER (ORDER BY N) AS CumulativeInterest,
+        SUM(@RepaymentAmt) OVER (ORDER BY N) AS CumulativeRepayment,
+        @BalanceAmt + SUM(@RepaymentAmt) OVER (ORDER BY N) AS RunningBalance
+    FROM InterestCalculations
+)
 
-        -- Update variables for next iteration
-        SET @IsFirstPayment = 0
-        SET @BalanceAmt = CASE
-            WHEN @ExpiryDate <= @NextPaymentDate THEN 0
-            ELSE @BalanceAmt
-        END
-        SET @Sequence += @RepayPeriodMths
-        SET @RepaymentInterest = 0.0
-    END
+-- Final result set
+INSERT INTO DW.CAU_RepaymentsSchedules_TEMP (FacilityId, RepaymentAmount, PrincipalAmount, PaymentDate)
+SELECT 
+    @FacilityId,
+    CASE
+        WHEN PaymentDate = @ExpiryDate THEN ABS(RunningBalance) + InterestAmount
+        WHEN RunningBalance >= 0 THEN 0
+        ELSE RepaymentAmount + InterestAmount
+    END AS RepaymentAmount,
+    CASE
+        WHEN RunningBalance >= 0 OR PaymentDate = @ExpiryDate THEN 0
+        ELSE LEAST(ABS(RunningBalance), RepaymentAmount)
+    END AS PrincipalAmount,
+    PaymentDate
+FROM Repayments
+WHERE RunningBalance < 0 OR PaymentDate = @ExpiryDate
+ORDER BY N;
 
-    SET @CurrentSequence += 1
-END
+-- Update final balance (if needed for further processing)
+UPDATE @BalanceAmt = (SELECT TOP 1 RunningBalance FROM Repayments ORDER BY N DESC);
